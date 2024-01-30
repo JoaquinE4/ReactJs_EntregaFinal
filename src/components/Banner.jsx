10b17bdf629ae7ef20fcf1bad33df230ed15e071
Joaco.dEx <casarotajoachin@gmail.com>
2024-01-30 14:46:18 -0300

    Joaquin Gallinal Cassarotti Pre_entrega 2

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,5 @@
 import banerSole from "../assets/baner.jpg"
-const Banner = ({tamaño}) => {
+const Banner = () => {
     return(
         <div className="container my-5 d-flex  aling-items-center justify-items-center"   data-aos-duration="700" data-aos="zoom-in"  >
             <img src={banerSole} alt="Gordita Boladora" className="center-block img-fluid" />
